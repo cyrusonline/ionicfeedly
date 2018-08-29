57622d4b396fbc77ef00fadf582c4d74f6a54990
Cyrus Chan <cyruschan@hsmc.edu.hk>
2018-08-29 14:33:56 +0800

    login using firebase authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,16 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { LoginPage } from '../pages/login/login';
 import { SignupPage } from '../pages/signup/signup';
-
+import firebase from 'firebase';
+var config = {
+  apiKey: "AIzaSyB9eiuN5GwxRpiceU_2X9U_D2kXRvzHccw",
+  authDomain: "cloudfunction-9d075.firebaseapp.com",
+  databaseURL: "https://cloudfunction-9d075.firebaseio.com",
+  projectId: "cloudfunction-9d075",
+  storageBucket: "cloudfunction-9d075.appspot.com",
+  messagingSenderId: "351452593314"
+};
+firebase.initializeApp(config)
 @NgModule({
   declarations: [
     MyApp,

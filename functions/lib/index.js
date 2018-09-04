b1d0b5bcbd501e632cbfadc3cc5df6623c06feb4
Cyrus Chan <cyruschan@hsmc.edu.hk>
2018-09-04 12:04:13 +0800

    implementing likes functionality in app

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,10 +1,38 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!");
+});
+exports.updateLikesCount = functions.https.onRequest((request, response) => {
+    console.log(request.body);
+    const postId = request.body.postId;
+    const userId = request.body.userId;
+    const action = request.body.action; // 'like' or 'unlike'
+    admin.firestore().collection("posts").doc(postId).get().then((data) => {
+        let likesCount = data.data().likesCount || 0;
+        let likes = data.data().likes || [];
+        let updateData = {};
+        if (action == "like") {
+            updateData["likesCount"] = ++likesCount;
+            updateData[`likes.${userId}`] = true;
+        }
+        else {
+            updateData["likesCount"] = --likesCount;
+            updateData[`likes.${userId}`] = false;
+        }
+        admin.firestore().collection("posts").doc(postId).update(updateData).then(() => {
+            response.status(200).send("Done");
+        }).catch((err) => {
+            response.status(err.code).send(err.message);
+        });
+    }).catch((err) => {
+        response.status(err.code).send(err.message);
+    });
 });
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,29 +8,36 @@ export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
 
-export const updateLikesCount = functions.https.onRequest((request,response)=>{
-    console.log(request.body)
+export const updateLikesCount = functions.https.onRequest((request, response) => {
+
+    console.log(request.body);
+
     const postId = request.body.postId;
     const userId = request.body.userId;
-    const action = request.body.action;
+    const action = request.body.action; // 'like' or 'unlike'
+    admin.firestore().collection("posts").doc(postId).get().then((data) => {
 
-    admin.firestore().collection("posts").doc(postId).get().then((data)=>{
         let likesCount = data.data().likesCount || 0;
         let likes = data.data().likes || [];
+
         let updateData = {};
-        if (action == "like") {
+
+        if(action == "like"){
             updateData["likesCount"] = ++likesCount;
             updateData[`likes.${userId}`] = true;
-        }else{
-            updateData["likesCount"] = ++likesCount;
-            updateData[`likes.${userId}`] = true;
+        } else {
+            updateData["likesCount"] = --likesCount;
+            updateData[`likes.${userId}`] = false;
         }
 
-        admin.firestore().collection("posts").doc(postId).update(updateData).then(()=>{
+        admin.firestore().collection("posts").doc(postId).update(updateData).then(() => {
             response.status(200).send("Done")
-        }).catch(err=>{
+        }).catch((err) => {
             response.status(err.code).send(err.message);
         })
 
+    }).catch((err) => {
+        response.status(err.code).send(err.message);
     })
+
 })
